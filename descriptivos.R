b1f5cfdfbd89a11347eac0792a17822b74c9138b
1608elissa <elissalogomx@gmail.com>
2023-04-18 10:37:55 -0600

    Sólo se hicieron los análisis descriptivos

--- descriptivos.R
+++ descriptivos.R
@@ -1,30 +1,36 @@
 library(plotrix)
 library(readxl)
+library(rstatix)
+library(tidyverse)
 
-ruta_archivo <- readxl_example("Demograficos.xlsx")
-data3 <- read_excel("Demograficos.xlsx",sheet = "All")
+
+data3 <- read_xlsx("Demograficos.xlsx",sheet = "All")
 
 
 data3$DECADA <- as.factor(data3$DECADA)
 data3$ESCOLARIDAD <-as.numeric(data3$ESCOLARIDAD)
-data3$IDEREESTADO <-as.numeric(data3$IDEREESTADO)
-data3$IDERERASGO <-as.numeric(data3$IDERERASGO)
+data3$IDARE_R_PUNTAJE <-as.numeric(data3$IDARE_R_PUNTAJE)
+data3$IDERE_R_PUNTAJE <-as.numeric(data3$IDERE_R_PUNTAJE)
+data3$IDARE_E_PUNTAJE <-as.numeric(data3$IDARE_E_PUNTAJE)
+data3$IDERE_E_PUNTAJE <-as.numeric(data3$IDERE_E_PUNTAJE)
+data3$SHIPLEY_TOT <-as.numeric(data3$SHIPLEY_TOT)
+data3$MoCA <-as.numeric(data3$MoCA)
 
 
-anova_test(SHIPLEY~DECADA, data=data3)
+anova_test(SHIPLEY_TOT~DECADA, data=data3)
 anova_test(ESCOLARIDAD~DECADA, data=data3)
-anova_test(MOCA~DECADA, data=data3)
 anova_test(EDAD~DECADA, data=data3)
 
-std.error(data3$SHIPLEY)
+std.error(data3$SHIPLEY_TOT)
 std.error(data3$ESCOLARIDAD)
-std.error(data3$MOCA)
+std.error(data3$MoCA)
 std.error(data3$EDAD)
 
-kruskal_test(IDAREESTADO~DECADA, data=data3)
-kruskal_test(IDARERASGO~DECADA, data=data3)
-kruskal_test(IDEREESTADO~DECADA, data=data3)
-kruskal_test(IDERERASGO~DECADA, data=data3)
+kruskal_test(MoCA~DECADA, data=data3)
+kruskal_test(IDARE_R_PUNTAJE~DECADA, data=data3)
+kruskal_test(IDERE_R_PUNTAJE~DECADA, data=data3)
+kruskal_test(IDARE_E_PUNTAJE~DECADA, data=data3)
+kruskal_test(IDERE_E_PUNTAJE~DECADA, data=data3)
 
 range(data3$IDAREESTADO, na.rm = T)
 range(data3$IDARERASGO, na.rm = T)
@@ -39,25 +45,46 @@ decadas<-c("20","30","40","50","60")
 for (i in decadas) {
   bases<- filter(data3,DECADA== i)
   print(i) 
-  std.error(bases$SHIPLEY)  %>%
+  std.error(bases$SHIPLEY_TOT)  %>%
   print()
   std.error(bases$ESCOLARIDAD)  %>%
   print()
-  std.error(bases$MOCA) %>%
+  range(bases$MoCA) %>%
   print()
   std.error(bases$EDAD) %>%
   print()
-  range(bases$IDAREESTADO, na.rm = T) %>%
+  range(bases$IDARE_R_PUNTAJE, na.rm = T) %>%
   print()
-  range(bases$IDARERASGO, na.rm = T) %>%
+  range(bases$IDERE_R_PUNTAJE, na.rm = T) %>%
   print()
-  range(bases$IDEREESTADO, na.rm = T) %>%
+  range(bases$IDARE_E_PUNTAJE, na.rm = T) %>%
   print()
-  range(bases$IDERERASGO, na.rm = T) %>%
+  range(bases$IDERE_E_PUNTAJE, na.rm = T) %>%
   print()
  #print(bases) 
 }
  
+for (i in decadas) {
+  bases<- filter(data3,DECADA== i)
+  print(i) 
+  mean(bases$SHIPLEY_TOT)  %>%
+    print()
+  mean(bases$ESCOLARIDAD)  %>%
+    print()
+  median(bases$MoCA) %>%
+    print()
+  mean(bases$EDAD) %>%
+    print()
+  median(bases$IDARE_R_PUNTAJE, na.rm = T) %>%
+    print()
+  median(bases$IDERE_R_PUNTAJE, na.rm = T) %>%
+    print()
+  median(bases$IDARE_E_PUNTAJE, na.rm = T) %>%
+    print()
+  median(bases$IDERE_E_PUNTAJE, na.rm = T) %>%
+    print()
+  #print(bases) 
+}
 
 
 
