74caca46e4c86c19c1585b9dc5adc2a19455c77b
1608elissa <elissalogomx@gmail.com>
2023-04-18 21:09:16 -0600

    Ahora sí logré hacer las regresiones o los anovas, sólo faltan las gráficas

--- anovas.R
+++ anovas.R
@@ -10,175 +10,94 @@ data <- read_xlsx("Conducta resultados.xlsx",sheet = "TR_RC")%>%
   separate(tip_cond_val, c("COND","TR_RC","TIPO", "VAL"),
            sep = "_")
 data$DECADA <- as.factor(data$DECADA)
+data$TR_RC <- as.factor(data$TR_RC)
 data$COND <- as.factor(data$COND)
 data$VAL <- as.factor(data$VAL)
 data$TIPO <- as.factor(data$TIPO)
 
-##### Respuestas correctas ####
+##### Respuestas correctas ###
 filter(data, TR_RC == "RC", TIPO=="TG", !VAL=="TOT") %>%
-  aov(value ~ COND*VAL*TIPO*DECADA + Error(ID/(COND*VAL*DECADA)), data=.)%>%
-  summary()
-
-filter(data, TR_RC == "RC", !VAL=="TOT") %>%
-  aov(value~COND*VAL*TIPO*DECADA, data=.)%>%
-  tukey_hsd()%>%
-  filter(p.adj < 0.05)%>%
-  View()
-
-filter(data, TR_RC == "RC", !VAL=="TOT") %>%
-  aov(value ~ COND*DECADA + Error(ID/(COND*DECADA)), data=.)%>%
-  summary()
-
-filter(data, TR_RC == "RC", !VAL=="TOT") %>%
-  aov(value~COND*DECADA, data=.)%>%
-  tukey_hsd()%>%
-  filter(p.adj < 0.05)%>%
-  View()
-
-##### Tiempo de reaccion 
-
-filter(data, TR_RC == "TR", !VAL=="TOT") %>%
-  aov(value ~ COND*VAL*DECADA + Error(ID/(COND*VAL*DECADA)), data=.) %>%
-  summary()
-
-filter(data, TR_RC == "TR", !VAL=="TOT") %>%
   aov(value ~ COND*VAL*DECADA, data=.)%>%
-  tukey_hsd() %>%
+  summary()
+
+filter(data, TR_RC == "RC", TIPO=="TG", !VAL=="TOT") %>%
+  aov(value~COND*VAL*DECADA, data=.)%>%
+  tukey_hsd()%>%
   filter(p.adj < 0.05)%>%
   View()
 
-filter(data, TR_RC == "TR", !VAL=="TOT") %>%
-  aov(value ~ COND*DECADA + Error(ID/(COND*DECADA)), data=.) %>%
+filter(data, TR_RC == "RC", TIPO=="NT", !VAL=="TOT") %>%
+  aov(value ~ COND*VAL*DECADA, data=.)%>%
   summary()
 
-filter(data, TR_RC == "TR", !VAL=="TOT") %>%
-  aov(value ~ COND*DECADA, data=.)%>%
-  tukey_hsd() %>%
+filter(data, TR_RC == "RC", TIPO=="NT", !VAL=="TOT") %>%
+  aov(value~COND*VAL*DECADA, data=.)%>%
+  tukey_hsd()%>%
   filter(p.adj < 0.05)%>%
   View()
 
-####### SOLO NEUTRALES #####
-data1 <- read_xlsx("data.xlsx")%>%
-  gather(tip_cond_val, value, -c("ID","DECADA", "SEXO","EDAD")) %>%
-  separate(tip_cond_val, c("TR_RC", "COND", "VAL"),
-           sep = " ")
-data1$DECADA <- as.factor(data1$DECADA)
-data1$COND <- as.factor(data1$COND)
+##### Tiempo de reaccion ###
 
-##### Respuestas correctas 
-
-filter(data1, TR_RC == "RC", VAL=="NEU") %>%
-  aov(value ~ COND*DECADA, data=.)%>%
+filter(data, TR_RC == "TR", TIPO=="TG", !VAL=="TOT") %>%
+  aov(value ~ COND*VAL*DECADA, data=.)%>%
   summary()
 
-filter(data1, TR_RC == "RC", VAL=="NEU") %>%
-  aov(value~COND*DECADA, data=.)%>%
+filter(data, TR_RC == "TR", TIPO=="TG", !VAL=="TOT") %>%
+  aov(value~COND*VAL*DECADA, data=.)%>%
+  tukey_hsd()%>%
+  filter(p.adj < 0.05)%>%
+  View()
+
+filter(data, TR_RC == "TR", TIPO=="NT", !VAL=="TOT") %>%
+  aov(value ~ COND*VAL*DECADA, data=.)%>%
+  summary()
+
+filter(data, TR_RC == "TR", TIPO=="NT", !VAL=="TOT") %>%
+  aov(value~COND*VAL*DECADA, data=.)%>%
   tukey_hsd()%>%
   filter(p.adj < 0.05)%>%
   View()
 
 
-##### Tiempo de reaccion 
+#### INDICE DE EFICIENCIA INVERSA ####
 
-filter(data1, TR_RC == "TR", VAL=="NEU") %>%
-  aov(value ~ COND*DECADA, data=.) %>%
-  summary()
-
-filter(data1, TR_RC == "TR", VAL=="NEU") %>%
-  aov(value ~ COND*DECADA, data=.)%>%
-  tukey_hsd() %>%
-  filter(p.adj < 0.05)%>%
-  View()
-
-
-####### MECANISMO AMPLIFICACION Y SUPRESION #####
-data2 <- read_xlsx("data.xlsx", sheet = "Hoja2")%>%
-  gather(tip_cond_val, value, -c("ID","DECADA")) %>%
-  separate(tip_cond_val, c("TR_RC", "IND", "VAL"),
-           sep = " ")
-data2$DECADA <- as.factor(data2$DECADA)
-data2$IND <- as.factor(data2$IND)
-
-
-#### Respuestas correctas
-filter(data2, TR_RC == "INRC", IND == "SUP", VAL=="NEU") %>%
-  aov(value ~ DECADA, data=.)%>%
-  summary()
-
-filter(data2, TR_RC == "INRC", VAL=="NEU") %>%
-  aov(value~IND*DECADA, data=.)%>%
-  tukey_hsd()%>%
-  filter(p.adj < 0.05)%>%
-  View()
-
-filter(data2, TR_RC == "INRC", IND == "AMP", VAL=="NEU") %>%
-  aov(value ~ DECADA, data=.)%>%
-  summary()
-
-filter(data2, TR_RC == "INRC", VAL=="NEU") %>%
-  aov(value~IND*DECADA, data=.)%>%
-  tukey_hsd()%>%
-  filter(p.adj < 0.05)%>%
-  View()
-
-
-#### Tiempos de reaccion
-filter(data2, TR_RC == "INTR", IND == "SUP", VAL=="NEU") %>%
-  aov(value ~ DECADA, data=.)%>%
-  summary()
-
-filter(data2, TR_RC == "INTR", VAL=="NEU") %>%
-  aov(value~IND*DECADA, data=.)%>%
-  tukey_hsd()%>%
-  filter(p.adj < 0.05)%>%
-  View()
-
-filter(data2, TR_RC == "INTR", IND == "AMP", VAL=="NEU") %>%
-  aov(value ~ DECADA, data=.)%>%
-  summary()
-
-filter(data2, TR_RC == "INTR", VAL=="NEU") %>%
-  aov(value~IND*DECADA, data=.)%>%
-  tukey_hsd()%>%
-  filter(p.adj < 0.05)%>%
-  View()
-
-#### Eficiencia inversa
-filter(data2, TR_RC == "INEI", IND == "SUP", VAL=="NEU") %>%
-  aov(value ~ DECADA, data=.)%>%
-  summary()
-
-filter(data2, TR_RC == "INEI", VAL=="NEU") %>%
-  aov(value~IND*DECADA, data=.)%>%
-  tukey_hsd()%>%
-  filter(p.adj < 0.05)%>%
-  View()
-
-filter(data2, TR_RC == "INEI", IND == "AMP", VAL=="NEU") %>%
-  aov(value ~ DECADA, data=.)%>%
-  summary()
-
-filter(data2, TR_RC == "INEI", VAL=="NEU") %>%
-  aov(value~IND*DECADA, data=.)%>%
-  tukey_hsd()%>%
-  filter(p.adj < 0.05)%>%
-  View()
-
-####### INDICE DE EFICIENCIA INVERSA #####
-data <- read_xlsx("data.xlsx",sheet = "Hoja4")%>%
-  gather(tip_cond_val, value, -c("ID","DECADA")) %>%
-  separate(tip_cond_val, c("EI", "COND", "VAL"),
-           sep = " ")
+data <- read_xlsx("Conducta resultados.xlsx",sheet = "EI")%>%
+  gather(tip_cond_val, value, -c("ID","DECADA", "SEXO", "EDAD")) %>%
+  separate(tip_cond_val, c("COND","EI","VAL"),
+           sep = "_")
 data$DECADA <- as.factor(data$DECADA)
 data$COND <- as.factor(data$COND)
+data$VAL <- as.factor(data$VAL)
 
 
-filter(data, EI == "EI", VAL=="NEU") %>%
-  aov(value ~ COND*DECADA, data=.)%>%
+filter(data) %>%
+  aov(value ~ COND*DECADA*VAL, data=.)%>%
   summary()
 
-filter(data,  EI == "EI", VAL=="NEU") %>%
-  aov(value~COND*DECADA, data=.)%>%
+filter(data) %>%
+  aov(value~COND*DECADA*VAL, data=.)%>%
+  tukey_hsd()%>%
+  filter(p.adj < 0.05)%>%
+  View()
+
+
+#### D PRIMA ####
+
+data <- read_xlsx("Conducta resultados.xlsx",sheet = "DPRIMA")%>%
+  gather(tip_cond_val, value, -c("ID","DECADA", "SEXO", "EDAD")) %>%
+  separate(tip_cond_val, c("DPRIMA","COND","VAL"),
+           sep = "_")
+data$DECADA <- as.factor(data$DECADA)
+data$COND <- as.factor(data$COND)
+data$VAL <- as.factor(data$VAL)
+
+
+filter(data) %>%
+  aov(value ~ COND*DECADA*VAL, data=.)%>%
+  summary()
+
+filter(data) %>%
+  aov(value~COND*DECADA*VAL, data=.)%>%
   tukey_hsd()%>%
   filter(p.adj < 0.05)%>%
   View()
--- regresiones.R
+++ regresiones.R
@@ -10,6 +10,8 @@ data <- read_xlsx("Conducta resultados.xlsx",sheet = "TR_RC")%>%
   separate(tip_cond_val, c("COND","TR_RC","TIPO", "VAL"),
            sep = "_")
 
+data$DECADA <- as.factor(data$DECADA)
+data$TR_RC <- as.factor(data$TR_RC)
 data$COND <- as.factor(data$COND)
 data$VAL <- as.factor(data$VAL)
 data$TIPO <- as.factor(data$TIPO)
@@ -20,13 +22,13 @@ filter(data, TR_RC == "RC", TIPO=="TG", !VAL=="TOT") %>%
   lm(formula= value ~ COND+VAL+EDAD, data =.)%>%
   summary()
 
-filter(data, COND=="PAS",TR_RC == "RC", TIPO=="TG", VAL=="NEU") %>%
+filter(data, COND=="ROS",TR_RC == "RC", TIPO=="TG", VAL=="NEU") %>%
   lm(formula= value ~ EDAD, data =.)%>%
   summary()
-filter(data, COND=="PAS",TR_RC == "RC", TIPO=="TG", VAL=="POS") %>%
+filter(data, COND=="ROS",TR_RC == "RC", TIPO=="TG", VAL=="POS") %>%
   lm(formula= value ~ EDAD, data =.)%>%
   summary()
-filter(data, COND=="PAS",TR_RC == "RC", TIPO=="TG", VAL=="NEG") %>%
+filter(data, COND=="ROS",TR_RC == "RC", TIPO=="TG", VAL=="NEG") %>%
   lm(formula= value ~ EDAD, data =.)%>%
   summary()
 
@@ -40,33 +42,13 @@ filter(data, COND=="ESC",TR_RC == "RC", TIPO=="TG", VAL=="NEG") %>%
   lm(formula= value ~ EDAD, data =.)%>%
   summary()
 
-filter(data, COND=="ROS",TR_RC == "RC", TIPO=="TG", VAL=="NEU") %>%
+filter(data, COND=="PAS",TR_RC == "RC", VAL=="NEU") %>%
   lm(formula= value ~ EDAD, data =.)%>%
   summary()
-filter(data, COND=="ROS",TR_RC == "RC", TIPO=="TG", VAL=="POS") %>%
+filter(data, COND=="PAS",TR_RC == "RC", VAL=="POS") %>%
   lm(formula= value ~ EDAD, data =.)%>%
   summary()
-filter(data, COND=="ROS",TR_RC == "RC", TIPO=="TG", VAL=="NEG") %>%
-  lm(formula= value ~ EDAD, data =.)%>%
-  summary()
-
-filter(data, COND=="PAS",TR_RC == "RC", TIPO=="NT", VAL=="NEU") %>%
-  lm(formula= value ~ EDAD, data =.)%>%
-  summary()
-filter(data, COND=="PAS",TR_RC == "RC", TIPO=="NT", VAL=="POS") %>%
-  lm(formula= value ~ EDAD, data =.)%>%
-  summary()
-filter(data, COND=="PAS",TR_RC == "RC", TIPO=="NT", VAL=="NEG") %>%
-  lm(formula= value ~ EDAD, data =.)%>%
-  summary()
-
-filter(data, COND=="ESC",TR_RC == "RC", TIPO=="NT", VAL=="NEU") %>%
-  lm(formula= value ~ EDAD, data =.)%>%
-  summary()
-filter(data, COND=="ESC",TR_RC == "RC", TIPO=="NT", VAL=="POS") %>%
-  lm(formula= value ~ EDAD, data =.)%>%
-  summary()
-filter(data, COND=="ESC",TR_RC == "RC", TIPO=="NT", VAL=="NEG") %>%
+filter(data, COND=="PAS",TR_RC == "RC", VAL=="NEG") %>%
   lm(formula= value ~ EDAD, data =.)%>%
   summary()
 
@@ -80,27 +62,72 @@ filter(data, COND=="ROS",TR_RC == "RC", TIPO=="NT", VAL=="NEG") %>%
   lm(formula= value ~ EDAD, data =.)%>%
   summary()
 
+filter(data, COND=="ESC",TR_RC == "RC", TIPO=="NT", VAL=="NEU") %>%
+  lm(formula= value ~ EDAD, data =.)%>%
+  summary()
+filter(data, COND=="ESC",TR_RC == "RC", TIPO=="NT", VAL=="POS") %>%
+  lm(formula= value ~ EDAD, data =.)%>%
+  summary()
+filter(data, COND=="ESC",TR_RC == "RC", TIPO=="NT", VAL=="NEG") %>%
+  lm(formula= value ~ EDAD, data =.)%>%
+  summary()
+
+
 ##### Tiempo de reaccion ###
 
 filter(data, TR_RC == "TR", TIPO=="TG", !VAL=="TOT") %>%
-  aov(value ~ COND*VAL*DECADA, data=.)%>%
+  lm(formula= value ~ COND+VAL+EDAD, data =.)%>%
   summary()
 
-filter(data, TR_RC == "TR", TIPO=="TG", !VAL=="TOT") %>%
-  aov(value~COND*VAL*DECADA, data=.)%>%
-  tukey_hsd()%>%
-  filter(p.adj < 0.05)%>%
-  View()
-
-filter(data, TR_RC == "TR", TIPO=="NT", !VAL=="TOT") %>%
-  aov(value ~ COND*VAL*DECADA, data=.)%>%
+filter(data, COND=="PAS",TR_RC == "TR", VAL=="NEU") %>%
+  lm(formula= value ~ EDAD, data =.)%>%
+  summary()
+filter(data, COND=="PAS",TR_RC == "TR", VAL=="POS") %>%
+  lm(formula= value ~ EDAD, data =.)%>%
+  summary()
+filter(data, COND=="PAS",TR_RC == "TR", VAL=="NEG") %>%
+  lm(formula= value ~ EDAD, data =.)%>%
   summary()
 
-filter(data, TR_RC == "TR", TIPO=="NT", !VAL=="TOT") %>%
-  aov(value~COND*VAL*DECADA, data=.)%>%
-  tukey_hsd()%>%
-  filter(p.adj < 0.05)%>%
-  View()
+filter(data, COND=="ESC",TR_RC == "TR", TIPO=="TG", VAL=="NEU") %>%
+  lm(formula= value ~ EDAD, data =.)%>%
+  summary()
+filter(data, COND=="ESC",TR_RC == "TR", TIPO=="TG", VAL=="POS") %>%
+  lm(formula= value ~ EDAD, data =.)%>%
+  summary()
+filter(data, COND=="ESC",TR_RC == "TR", TIPO=="TG", VAL=="NEG") %>%
+  lm(formula= value ~ EDAD, data =.)%>%
+  summary()
+
+filter(data, COND=="ROS",TR_RC == "TR", TIPO=="TG", VAL=="NEU") %>%
+  lm(formula= value ~ EDAD, data =.)%>%
+  summary()
+filter(data, COND=="ROS",TR_RC == "TR", TIPO=="TG", VAL=="POS") %>%
+  lm(formula= value ~ EDAD, data =.)%>%
+  summary()
+filter(data, COND=="ROS",TR_RC == "TR", TIPO=="TG", VAL=="NEG") %>%
+  lm(formula= value ~ EDAD, data =.)%>%
+  summary()
+
+filter(data, COND=="ESC",TR_RC == "TR", TIPO=="NT", VAL=="NEU") %>%
+  lm(formula= value ~ EDAD, data =.)%>%
+  summary()
+filter(data, COND=="ESC",TR_RC == "TR", TIPO=="NT", VAL=="POS") %>%
+  lm(formula= value ~ EDAD, data =.)%>%
+  summary()
+filter(data, COND=="ESC",TR_RC == "TR", TIPO=="NT", VAL=="NEG") %>%
+  lm(formula= value ~ EDAD, data =.)%>%
+  summary()
+
+filter(data, COND=="ROS",TR_RC == "TR", TIPO=="NT", VAL=="NEU") %>%
+  lm(formula= value ~ EDAD, data =.)%>%
+  summary()
+filter(data, COND=="ROS",TR_RC == "TR", TIPO=="NT", VAL=="POS") %>%
+  lm(formula= value ~ EDAD, data =.)%>%
+  summary()
+filter(data, COND=="ROS",TR_RC == "TR", TIPO=="NT", VAL=="NEG") %>%
+  lm(formula= value ~ EDAD, data =.)%>%
+  summary()
 
 
 #### INDICE DE EFICIENCIA INVERSA ####
@@ -113,17 +140,35 @@ data$DECADA <- as.factor(data$DECADA)
 data$COND <- as.factor(data$COND)
 data$VAL <- as.factor(data$VAL)
 
-
-filter(data) %>%
-  aov(value ~ COND*DECADA*VAL, data=.)%>%
+filter(data, COND=="PAS", VAL=="NEU") %>%
+  lm(formula= value ~ EDAD, data =.)%>%
+  summary()
+filter(data, COND=="PAS", VAL=="NEG") %>%
+  lm(formula= value ~ EDAD, data =.)%>%
+  summary()
+filter(data, COND=="PAS", VAL=="POS") %>%
+  lm(formula= value ~ EDAD, data =.)%>%
   summary()
 
-filter(data) %>%
-  aov(value~COND*DECADA*VAL, data=.)%>%
-  tukey_hsd()%>%
-  filter(p.adj < 0.05)%>%
-  View()
+filter(data, COND=="ROS", VAL=="NEU") %>%
+  lm(formula= value ~ EDAD, data =.)%>%
+  summary()
+filter(data, COND=="ROS", VAL=="NEG") %>%
+  lm(formula= value ~ EDAD, data =.)%>%
+  summary()
+filter(data, COND=="ROS", VAL=="POS") %>%
+  lm(formula= value ~ EDAD, data =.)%>%
+  summary()
 
+filter(data, COND=="ESC", VAL=="NEU") %>%
+  lm(formula= value ~ EDAD, data =.)%>%
+  summary()
+filter(data, COND=="ESC", VAL=="NEG") %>%
+  lm(formula= value ~ EDAD, data =.)%>%
+  summary()
+filter(data, COND=="ESC", VAL=="POS") %>%
+  lm(formula= value ~ EDAD, data =.)%>%
+  summary()
 
 #### D PRIMA ####
 
@@ -131,20 +176,31 @@ data <- read_xlsx("Conducta resultados.xlsx",sheet = "DPRIMA")%>%
   gather(tip_cond_val, value, -c("ID","DECADA", "SEXO", "EDAD")) %>%
   separate(tip_cond_val, c("DPRIMA","COND","VAL"),
            sep = "_")
+
 data$DECADA <- as.factor(data$DECADA)
 data$COND <- as.factor(data$COND)
 data$VAL <- as.factor(data$VAL)
 
-
-filter(data) %>%
-  aov(value ~ COND*DECADA*VAL, data=.)%>%
+filter(data, COND=="ROSTROS", VAL=="NEU") %>%
+  lm(formula= value ~ EDAD, data =.)%>%
+  summary()
+filter(data, COND=="ROSTROS", VAL=="NEG") %>%
+  lm(formula= value ~ EDAD, data =.)%>%
+  summary()
+filter(data, COND=="ROSTROS", VAL=="POS") %>%
+  lm(formula= value ~ EDAD, data =.)%>%
   summary()
 
-filter(data) %>%
-  aov(value~COND*DECADA*VAL, data=.)%>%
-  tukey_hsd()%>%
-  filter(p.adj < 0.05)%>%
-  View()
+filter(data, COND=="ESCENAS", VAL=="NEU") %>%
+  lm(formula= value ~ EDAD, data =.)%>%
+  summary()
+filter(data, COND=="ESCENAS", VAL=="NEG") %>%
+  lm(formula= value ~ EDAD, data =.)%>%
+  summary()
+filter(data, COND=="ESCENAS", VAL=="POS") %>%
+  lm(formula= value ~ EDAD, data =.)%>%
+  summary()
+
 
 
 ##### Respuestas correctas 
@@ -163,76 +219,10 @@ filter(data1, COND=="ROSTRO", VAL=="NEU") %>%
   geom_smooth(method = "lm", se = FALSE, color= "black") +
   theme_classic()
 
-filter(data1, COND=="ESCENA", VAL=="NEU") %>%
-  ggplot(aes(x = EDAD, y = value)) +
-  geom_point() + 
-  labs(x = "EDAD", y = "%RC") +  
-  geom_smooth(method = "lm", se = FALSE, color= "black") +
-  theme_classic()
 
-filter(data1, COND=="PASIV", VAL=="NEU") %>%
-  ggplot(aes(x = EDAD, y = value)) +
-  geom_point() + 
-  labs(x = "EDAD", y = "%RC") +  
-  geom_smooth(method = "lm", se = FALSE, color= "black") +
-  coord_cartesian(ylim = c(75,100)) +
-  theme_classic()
+data <- read_xlsx("analisis ale.xlsx")
+df <- data.frame(x = "TimeOpenArms", y = "Breakpoint", grupo = "Group")
+modelo <- lm(data$TimeOpenArms ~ data$Breakpoint)
 
-
-filter(data1, COND=="ROSTRO", VAL=="NEU") %>%
-  lm(value ~ EDAD * ESCOLARIDAD, data = .) %>% 
-  summary()
-
-filter(data1, COND=="ESCENA", VAL=="NEU") %>%
-  lm(value ~ EDAD * ESCOLARIDAD, data = .) %>% 
-  summary()
-
-filter(data1, COND=="PASIV", VAL=="NEU") %>%
-  lm(value ~ EDAD * ESCOLARIDAD, data = .) %>% 
-  summary()
-
-
-##### Tiempo de reaccion 
-
-data1 <- read_xlsx("TR_RC.xlsx", sheet = "Hoja2")%>%
-  gather(tip_cond_val, value, -c("ID","DECADA", "SEXO","EDAD","ESCOLARIDAD")) %>%
-  separate(tip_cond_val, c("TR_RC", "COND", "VAL"),
-           sep = " ")
-data1$DECADA <- as.factor(data1$DECADA)
-data1$COND <- as.factor(data1$COND)
-data1$ESCOLARIDAD <- as.numeric(data1$ESCOLARIDAD)
-
-filter(data1, COND=="ROSTRO", VAL=="NEU") %>%
-  ggplot(aes(x = EDAD, y = value)) +
-  geom_point() + 
-  theme_classic() +
-  labs(x = "EDAD", y = "TR") +  
-  geom_smooth(method = "lm", se = FALSE, color= "black") +
-  theme_classic()
-
-filter(data1, COND=="ESCENA", VAL=="NEU") %>%
-  ggplot(aes(x = EDAD, y = value)) +
-  geom_point() + 
-  labs(x = "EDAD", y = "TR") +  
-  geom_smooth(method = "lm", se = FALSE, color= "black") +
-  theme_classic()
-
-filter(data1, COND=="PASIV", VAL=="NEU") %>%
-  ggplot(aes(x = EDAD, y = value)) +
-  geom_point() + 
-  labs(x = "EDAD", y = "TR") +  
-  geom_smooth(method = "lm", se = FALSE, color= "black") +
-  theme_classic()
-
-
-filter(data1, COND=="ROSTRO", VAL=="NEU") %>%
-  lm(value ~ EDAD * ESCOLARIDAD, data = .) %>% 
-  summary()
-
-filter(data1, COND=="ESCENA", VAL=="NEU") %>%
-  lm(value ~ EDAD * ESCOLARIDAD, data = .) %>% 
-  summary()
-
-filter(data1, COND=="PASIV", VAL=="NEU") %>%
-  lm(value ~ EDAD * ESCOLARIDAD, data = .) %>% 
-  summary()
+ggplot(data, aes(x = TimeOpenArms, y = Breakpoint, color = Group)) + 
+  geom_point(size = 5) + geom_smooth(method = "lm", se= F) 
