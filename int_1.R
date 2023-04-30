966b1dd6dac83a95199e9916fe97a4992a722d1c
1608elissa <elissalogomx@gmail.com>
2023-04-29 21:24:12 -0600

    Se hicieron adaptaciones para ver como separar las variables de conjunto de datos en grupos iguales, Elipequeña propuso la funcion cut, no fue de utilidad, buscar otra funcion

---
+++ int_1.R
@@ -0,0 +1,35 @@
+library(tidyverse)
+
+a <- rnorm(200,100,1)
+a %<>% cut(7)
+
+a
+View(a)
+#######
+library(readxl)
+dat <- read_excel("mineria de datos/conjunto_datos25abril2023.xlsx")
+a<-dat$AB17
+
+levels(a)
+levels(a) <- seq(length(levels(a)))
+tab<-tibble("AB17"=a,
+            "2"=dat$AB17)
+view(tab)           
+
+split(a, sort(a%%15))
+
+
+library(openxlsx)
+
+wb<-createWorkbook("cosito")
+addWorksheet(wb,"labels")
+
+
+
+
+
+
+
+writeDataTable(wb, "labels", tab)
+openXL(wb)
+saveWorkbook(wb, "labes.xlsx", overwrite = TRUE)
