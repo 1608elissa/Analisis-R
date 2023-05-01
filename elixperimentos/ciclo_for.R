a58c8f22e9a491175268b8783c3c1e6fd7802b95
1608elissa <elissalogomx@gmail.com>
2023-05-01 17:11:05 -0600

    Se añade carpeta para los experimentos de como hacer cosas y se añade archivo for donde se resuleve el hacer los ciclos for para automatizar todo a futuro con las mil8mil variables

---
+++ elixperimentos/ciclo_for.R
@@ -0,0 +1,6 @@
+i <- c("AC24est","AB15","AB17")
+
+for (n in i) {
+  dat[[n]]
+
+}
--- elixperimentos/int_1.R
+++ elixperimentos/int_1.R
@@ -41,3 +41,21 @@ saveWorkbook(wb, "labes.xlsx", overwrite = TRUE)
 #2. modificar las variables en el excel para poder generar rangos
 #   de manera automatica y no queden lejos del ultimo valor perdido
 
+
+#pruebas####
+
+
+
+library(readxl)
+dat <- read_excel("mineria de datos/conjunto_datos25abril2023.xlsx")
+dat$AC24est  %<>%  as.numeric()
+a<-dat$AC24est %>%
+  cut(10)
+b<-a
+
+levels(a)
+levels(b) <- seq(length(levels(a)))
+tab<-tibble("AC24est"=a,
+            "1"=b,
+            "2"=dat$AC24est)
+view(tab)           
