4155e621f4a5ae497eeb3772937feb29e6ccb879
1608elissa <elissalogomx@gmail.com>
2023-05-01 20:45:10 -0600

    Se hicieron las categorias. Se establecieron 10 categorias y se remplazaron los NA por 666

--- elixperimentos/intento1.R
+++ elixperimentos/intento1.R
@@ -5,24 +5,37 @@ library(openxlsx)
 
 dat <- read_excel("mineria de datos/conjunto_datos25abril2023.xlsx")
 list<- pull(read_excel("mineria de datos/conjunto_datos25abril2023.xlsx",sheet = 2),2)
-i <- list[1:34]
+i <- list[1:33]
+
+wb<-createWorkbook("cosito")
 
 
 for (n in i) {
   #print(dat[[n]])
   
-  a<-dat[[n]] %>%
+  a<-dat[[n]]
+  
+  b <- a %>%
     cut(10)
-  b <- a
+  c <- b
   
-  levels(a)
-  levels(a) <- seq(length(levels(a)))
-  tab<-tibble( as.name(n)=a,
-              var1 = b,
-              var2 =dat[[n]])
-  view(tab)           
+  levels(b)
+  levels(b) <- seq(length(levels(b)))
+  tab<-tibble( n=as.numeric(a),
+              varb = as.numeric(b),
+              varc = as.numeric(c))
   
+  tab[is.na(tab)] <- 666
+  #tab <- tab %>% replace(is.na(.), as.factor(666))
+
+ # view(tab)           
+  addWorksheet(wb,n)
+  writeDataTable(wb, n, tab)
+
 }
+
+openXL(wb) #abrir sin guardar
+saveWorkbook(wb, "labes.xlsx", overwrite = TRUE) #guardar sin abrir
 
 
 
