2618773379e830c2e5785b8aa90faa2a97a2eec3
1608elissa <elissalogomx@gmail.com>
2023-05-11 22:35:54 -0600

    cositos raros

--- elixperimentos/Nom_var_unq.R
+++ elixperimentos/Nom_var_unq.R
@@ -1,5 +1,6 @@
-mod<- read_excel("mineria de datos/conjunto_datos25abril2023.xlsx",sheet = "Modelo")
-
+mod<- read_excel("mineria de datos/conjunto_datos25abril2023.xlsx",sheet = "cubetas")
+filter(mod,AD33==1) %>%
+  view()
 
 wb<-createWorkbook("noms")
 
@@ -9,6 +10,22 @@ for (i in names(mod)) {
   addWorksheet(wb, i)
   writeDataTable(wb, i,tab)
     
+}
+
+saveWorkbook(wb, "mineria de datos/MagiaDeSirena.xlsx", overwrite = TRUE) #guardar sin abrir
+
+
+
+
+
+wb<-createWorkbook("noms")
+
+for (i in names(mod)) {
+  
+  tab <- as.data.frame(table(mod[i]))
+  addWorksheet(wb, i)
+  writeDataTable(wb, i,tab)
+  
 }
 
 saveWorkbook(wb, "mineria de datos/MagiaDeSirena.xlsx", overwrite = TRUE) #guardar sin abrir
