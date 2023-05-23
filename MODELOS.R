7a75c83ba40d9b04c785a9fa31f63c9e5ee29123
1608elissa <elissalogomx@gmail.com>
2023-05-23 15:31:44 -0600

    Intenté trabajar :c

--- MODELOS.R
+++ MODELOS.R
@@ -1,3 +1,27 @@
+library(readxl)
+library(rstatix)
+library(tidyverse)
+library(magrittr)
+
+
+#### BASE DE DATOS CORRELACIONES ####
+data <- read_xlsx("Junto.xlsx",sheet = "Correlaciones")%>%
+  gather(cond_tip_est_val, value, -c("ID","DECADA", "SEXO", "EDAD", "MoCA", "ESCOLARIDAD", 
+                                     "CRI_Total", "IDARE_R_PUNTAJE", "IDERE_R_PUNTAJE",
+                                     "COVID_CAT", "SUEÑO_NOR", "AMP_DUR_ROS", "AMP_DUR_ESC",
+                                     "SUP_DUR_ROS", "SUP_DUR_ESC", "AMP_1DU_ROS",
+                                     "AMP_1DU_ESC", "SUP_1DU_ROS", "SUP_1DU_ESC")) %>%
+  separate(cond_tip_est_val, c("COND","VD","TIPO", "VAL"), sep = "_")
+
+data$DECADA <- as.factor(data$DECADA)
+data$SEXO <- as.factor(data$SEXO)
+data$VD <- as.factor(data$VD)
+data$COND <- as.factor(data$COND)
+data$VAL <- as.factor(data$VAL)
+data$TIPO <- as.factor(data$TIPO)
+
+
+#### INDICE DE EFICIENCIA ####
 a <- filter(data, VD == "EI", COND== "ROS", VAL=="TOT", TIPO=="TG")
 
 b <- lm(formula= value ~ EDAD*ESCOLARIDAD*MoCA*CRI_Total, data = a)
@@ -19,7 +43,7 @@ modeloEIESC <- (lm(formula = value ~ EDAD * ESCOLARIDAD * MoCA * CRI_Total, data
 summary(modeloEIESC)
 
 
-
+#### INDICES DE AMPLIFICACION ####
 e <- filter(data, COND== "AMP", VD == "DUR", VAL=="TOT", TIPO=="ROS")
 
 f <- lm(formula= value ~ EDAD*ESCOLARIDAD*MoCA*CRI_Total, data = e)
@@ -30,7 +54,7 @@ modeloAMPDUR <- (lm(formula = value ~ ESCOLARIDAD + MoCA + ESCOLARIDAD:MoCA, dat
 summary(modeloAMPDUR)
 
 
-
+#### INDICES DE SUPRESION ####
 g <- filter(data, COND== "SUP", VD == "DUR", VAL=="TOT", TIPO=="ROS")
 
 h <- lm(formula= value ~ EDAD*ESCOLARIDAD*MoCA*CRI_Total, data = g)
@@ -60,6 +84,7 @@ filter(data, COND== "SUP", VD == "DUR", VAL=="TOT", TIPO=="ROS") %>%
   labs(x = "CRI_Total", y = "SUP")
 
 
+#### D PRIMA ####
 i <- filter(data, VD == "DPR", COND== "DP", VAL=="TOT", TIPO=="ROSTROS")
 
 j <- lm(formula= value ~ EDAD*ESCOLARIDAD*MoCA*CRI_Total, data = i)
